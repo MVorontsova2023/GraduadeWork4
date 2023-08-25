48160908a138afab790396bd034738453a55186f
Marina Vorontsova <mvorontsova2010@gmail.com>
2023-08-25 19:44:25 +0200

    Change BasePageTest.java

--- src/test/java/ui/tests/BasePageTest.java
+++ src/test/java/ui/tests/BasePageTest.java
@@ -19,7 +19,7 @@ class BasePageTest {
 //    public final String BASE_URL = "https://erich416.softr.app";
     public final String STUDENT_EMAIL = "malik@example.com";
     public final String STUDENT_PASSWORD = "123456";
-    public final String TEACHER_EMAIL = "maria@example.com";
+    public final String TEACHER_EMAIL = "mv@example.com";
     public final String TEACHER_PASSWORD = "123456789";
 
     @BeforeAll
